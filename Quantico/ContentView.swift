822e543396fdf5ade5de34fa24df2e3a5e9e0e9e
Yaochen Liu <yaochenliu@microsoft.com>
2024-06-05 17:21:27 +0800

    Initial Commit

---
+++ Quantico/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quantico
+//
+//  Created by Yaochen Liu on 2024/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quantico/QuanticoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuanticoApp.swift
+//  Quantico
+//
+//  Created by Yaochen Liu on 2024/6/5.
+//
+
+import SwiftUI
+
+@main
+struct QuanticoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
